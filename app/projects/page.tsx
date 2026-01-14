a01821370341bf295de50ea76b0e02781534f525
Roger Flores <rflores3113@gmail.com>
2026-01-13 18:14:24 -0800

    add about and projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-4">Projects</h1>
+      <p className="text-xl text-gray-600">WIP</p>
+    </main>
+  );
+}
